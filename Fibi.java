786bb857047e3b33a0e61d8263ed9f33a53c41ba
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2022-10-25 14:51:36 +0530

    Update Fibi.java

--- Fibi.java
+++ Fibi.java
@@ -7,12 +7,12 @@ class Fibi
 		System.out.println("Enter limit ");
 		int a = sc.nextInt();
 		int f=0,s=1;
-		System.out.println("Fibinocii series is " + f + s + "\t");
-		
-		
+		System.out.println("Fibinocii series is ");
+		System.out.println(f);
+		System.out.println(s);
 		for(int i=3;i<=a;i++)
 		{
-			System.out.println("\t");
+			
 			int t = f + s;
 			System.out.println(t);
 			f=s;
